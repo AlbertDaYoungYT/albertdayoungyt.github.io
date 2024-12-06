4f291bf9e48e37ef0ee548d90db20f44df3cb749
AlbertDaYoungYT <kcobain465@gmail.com>
2024-12-06 20:07:46 +0100

    Main page created

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "electric-violet": "#572EF5",
+        "port-gore": "#252146",
+        "lavender-purple": "#847BB5",
+        "mirage": "#181625",
+      },
+    },
+  },
+};
